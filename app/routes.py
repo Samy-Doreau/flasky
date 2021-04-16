4e0d33c7f83bb3f43535138439fdfc5c88cd1b36
Samy Doreau <samy.doreau@avanade.com>
2021-04-16 16:51:28 +0100

    Changes

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,31 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
+from app.forms import LoginForm
+
+
 @app.route('/')
 @app.route('/index')
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, rememember_me = {}'.format(
+            form.username.data, form.remember_me.data
+        ))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
+
+
 def index():
-    user  = {'username':'Miguel'}
+    user = {'username': 'Miguel'}
     posts = [
         {
-            'author':{'username':'John'},
-            'body':'A story about penguins'
+            'author': {'username': 'John'},
+            'body': 'A story about penguins'
         },
         {
-            'author':{'username':'Anla'},
-            'body':'Once upon a time in the bleast'
+            'author': {'username': 'Anla'},
+            'body': 'Once upon a time in the bleast'
         }
     ]
-    return render_template('index.html', title='Home',user = user, posts = posts)
+    return render_template('index.html', title='Home', user=user, posts=posts)
