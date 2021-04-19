d7295ec5dabcd324b187e969d5752e789a498ea0
Samy Doreau <samy.doreau@avanade.com>
2021-04-19 11:35:00 +0100

    Changed - db

--- app/routes.py
+++ app/routes.py
@@ -4,18 +4,18 @@ from app.forms import LoginForm
 
 
 @app.route('/')
-@app.route('/index')
-@app.route('/login', methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login requested for user {}, rememember_me = {}'.format(
             form.username.data, form.remember_me.data
         ))
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 
 
+@app.route('/index')
 def index():
     user = {'username': 'Miguel'}
     posts = [
