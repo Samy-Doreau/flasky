15d222ee682c6e68bb8a189fffa6c91f2fd36863
Samy Doreau <samy.doreau@avanade.com>
2021-04-20 13:26:37 +0100

    login modifs

--- app/routes.py
+++ app/routes.py
@@ -1,24 +1,35 @@
 from app import app
 from flask import render_template, flash, redirect
+from flask_login import current_user, login_user
+from app.models import User
 from app.forms import LoginForm
 
 
 @app.route('/')
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, rememember_me = {}'.format(
-            form.username.data, form.remember_me.data
-        ))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid User Name or Password'
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 
 
-@app.route('/index')
+@ app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@ app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
-    posts = [
+    user={'username': 'Miguel'}
+    posts=[
         {
             'author': {'username': 'John'},
             'body': 'A story about penguins'
